c55dc8df7f435af57a14514fc256866684a806a7
Jeff Carron <jeffrey.carron@gmail.com>
2022-02-10 23:23:57 -0500

    updated to bypass port when using on board network

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 const lti = require('ltijs').Provider
 
 //debug
-const indexDebug = require('debug')('provider:index');
-indexDebug('in index.js')
+//const indexDebug = require('debug')('provider:index');
+//indexDebug('in index.js')
 
 // Setup
-lti.setup(process.env.LTI_KEY,
+lti.setup(process.env.LTI_KEY1,
   {
     url: 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME,
     connection: { user: process.env.DB_USER, pass: process.env.DB_PASS }
@@ -21,24 +21,25 @@ lti.setup(process.env.LTI_KEY,
   		key: fs.readFileSync(path.resolve(__dirname, "../../ssl.key"), 'utf8')
 	},
     staticPath: path.join(__dirname, './public'), // Path to static files
-	appRoute: '/bsi2/', loginRoute: '/login', // Optionally, specify some of the reserved routes
+	appRoute: '/bsi2', loginRoute: '/login', // Optionally, specify some of the reserved routes
     cookies: {
       secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
       sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
     },
-    devMode: false // Set DevMode to true if the testing platform is in a different domain and https is not being used
+    devMode: false, // Set DevMode to true if the testing platform is in a different domain and https is not being used
+	cors: true
   })
 
 // When receiving successful LTI launch redirects to app
 lti.onConnect(async (token, req, res) => {
-  indexDebug('index.js -> onConnect')
+//  indexDebug('index.js -> onConnect')
   return res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 // When receiving deep linking request redirects to deep screen
 lti.onDeepLinking(async (token, req, res) => {
-  indexDebug('index.js -> onDeepLinking')
-  return lti.redirect(res, '/deeplink', { newResource: true })
+//  indexDebug('index.js -> onDeepLinking')
+  return lti.redirect(res, '/deeplink', { newResource: false })
 })
 
 // Setting up routes
@@ -46,32 +47,30 @@ lti.app.use(routes)
 
 // Setup function
 const setup = async () => {
-  indexDebug('index.js -> setup')
+//  indexDebug('index.js -> setup')
   await lti.deploy({ port: process.env.PORT })
 
   /**
    * Register platform
    */
-  /* await lti.registerPlatform({
-    url: 'http://localhost/moodle',
-    name: 'Platform',
-    clientId: 'CLIENTID',
-    authenticationEndpoint: 'http://localhost/moodle/mod/lti/auth.php',
-    accesstokenEndpoint: 'http://localhost/moodle/mod/lti/token.php',
-    authConfig: { method: 'JWK_SET', key: 'http://localhost/moodle/mod/lti/certs.php' }
-  }) */
-	
-  /**
-   * Register platform
-   */
   await lti.registerPlatform({
     url: 'https://smcdsbtest.desire2learn.com',
-    name: 'D2l Brightspace',
-    clientId: 'd169ca20-eead-42bd-89e0-5fbe2c1d4f30',
+    name: 'D2l Brightspace Testing Server',
+    clientId: process.env.LTI_KEY1,
     authenticationEndpoint: 'https://smcdsbtest.desire2learn.com/d2l/lti/authenticate',
     accesstokenEndpoint: 'https://auth.brightspace.com/core/connect/token',
     authConfig: { method: 'JWK_SET', key: 'https://smcdsbtest.desire2learn.com/d2l/.well-known/jwks' }
   }) 
+	
+  await lti.registerPlatform({
+    url: 'https://smcdsb.elearningontario.ca',
+    name: 'D2l Brightspace Production Server',
+    clientId: process.env.LTI_KEY2,
+    authenticationEndpoint: 'https://smcdsb.elearningontario.ca/d2l/lti/authenticate',
+    accesstokenEndpoint: 'https://auth.brightspace.com/core/connect/token',
+    authConfig: { method: 'JWK_SET', key: 'https://smcdsb.elearningontario.ca/d2l/.well-known/jwks' }
+  }) 
+	
 }
-
+//indexDebug('in index.js - before setup')
 setup()
--- src/routes.js
+++ src/routes.js
@@ -2,89 +2,76 @@ const router = require('express').Router()
 const path = require('path')
 
 //debug
-const routesDebug = require('debug')('provider:routes');
-routesDebug('in routes.js')
+//const routesDebug = require('debug')('provider:routes');
+//routesDebug('in routes.js')
 
 // Requiring Ltijs
 const lti = require('ltijs').Provider
 
 // Grading route
-router.post('/grade', async (req, res) => {
-  try {
-    const idtoken = res.locals.token // IdToken
-    const score = req.body.grade // User numeric score sent in the body
-    // Creating Grade object
-    const gradeObj = {
-      userId: idtoken.user,
-      scoreGiven: score,
-      scoreMaximum: 100,
-      activityProgress: 'Completed',
-      gradingProgress: 'FullyGraded'
-    }
-
-    // Selecting linetItem ID
-    let lineItemId = idtoken.platformContext.endpoint.lineitem // Attempting to retrieve it from idtoken
-    if (!lineItemId) {
-      const response = await lti.Grade.getLineItems(idtoken, { resourceLinkId: true })
-      const lineItems = response.lineItems
-      if (lineItems.length === 0) {
-        // Creating line item if there is none
-        console.log('Creating new line item')
-        const newLineItem = {
-          scoreMaximum: 100,
-          label: 'Grade',
-          tag: 'grade',
-          resourceLinkId: idtoken.platformContext.resource.id
-        }
-        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem)
-        lineItemId = lineItem.id
-      } else lineItemId = lineItems[0].id
-    }
-
-    // Sending Grade
-    const responseGrade = await lti.Grade.submitScore(idtoken, lineItemId, gradeObj)
-    return res.send(responseGrade)
-  } catch (err) {
-    console.log(err.message)
-    return res.status(500).send({ err: err.message })
-  }
-})
+//router.post('/grade', async (req, res) => {
+//  try {
+//    const idtoken = res.locals.token // IdToken
+//    const score = req.body.grade // User numeric score sent in the body
+//    // Creating Grade object
+//    const gradeObj = {
+//      userId: idtoken.user,
+//      scoreGiven: score,
+//      scoreMaximum: 100,
+//      activityProgress: 'Completed',
+//      gradingProgress: 'FullyGraded'
+//    }
+//
+//    // Selecting linetItem ID
+//    let lineItemId = idtoken.platformContext.endpoint.lineitem // Attempting to retrieve it from idtoken
+//    if (!lineItemId) {
+//      const response = await lti.Grade.getLineItems(idtoken, { resourceLinkId: true })
+//      const lineItems = response.lineItems
+//      if (lineItems.length === 0) {
+//        // Creating line item if there is none
+//        console.log('Creating new line item')
+//        const newLineItem = {
+//          scoreMaximum: 100,
+//          label: 'Grade',
+//          tag: 'grade',
+//          resourceLinkId: idtoken.platformContext.resource.id
+//        }
+//        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem)
+//        lineItemId = lineItem.id
+//      } else lineItemId = lineItems[0].id
+//    }
+//
+//    // Sending Grade
+//    const responseGrade = await lti.Grade.submitScore(idtoken, lineItemId, gradeObj)
+//    return res.send(responseGrade)
+//  } catch (err) {
+//    console.log(err.message)
+//    return res.status(500).send({ err: err.message })
+//  }
+//})
 
 // Names and Roles route
-router.get('/members', async (req, res) => {
-  try {
-    const result = await lti.NamesAndRoles.getMembers(res.locals.token)
-    if (result) return res.send(result.members)
-    return res.sendStatus(500)
-  } catch (err) {
-    console.log(err.message)
-    return res.status(500).send(err.message)
-  }
-})
+//router.get('/members', async (req, res) => {
+//  try {
+//    const result = await lti.NamesAndRoles.getMembers(res.locals.token)
+//    if (result) return res.send(result.members)
+//    return res.sendStatus(500)
+//  } catch (err) {
+//    console.log(err.message)
+//    return res.status(500).send(err.message)
+//  }
+//})
 
 // Deep linking route (2nd)
-router.post('/deeplink', async (req, res) => {
+router.post('/bsi2/deeplink', async (req, res) => {
   try {
     const resource = req.body
-	routesDebug('routes.js -> /deeplink')
+	//routesDebug('routes.js -> post /deeplink')
 	//routesDebug('req: \n')
 	//routesDebug(req)
 	//routesDebug('res: \n')
 	//routesDebug(res)
 
-//    const items = {
-//      type: 'ltiResourceLink',
-//      title: 'Ltijs Demo',
-//      custom: {
-//        name: resource.name,
-//        value: resource.value
-//      }
-//	const items = {
-//      type: 'html',
-//	  html: '<a href="https://www.google.ca/" target="_blank" style="background-color: #1c87c9; border: none; color: white; padding: 20px 34px; text-align: center; text-decoration: none; display: inline-block; font-size: 20px; margin: 4px 2px; cursor: pointer; border-radius: 10px;" rel="noopener">Google</a>',
-//      title: 'Custom Button',
-//      text: 'This tool inserts a custom button'
-//    }
 	const style = 'width: ' + resource.buttonStyle.width + 
 		'; height: ' + resource.buttonStyle.height + 
         '; background-color: ' + resource.buttonStyle.backgroundColor +
@@ -92,6 +79,7 @@ router.post('/deeplink', async (req, res) => {
         '; color: ' + resource.buttonStyle.color +
         '; border-radius: ' + resource.buttonStyle.borderRadius +
         '; padding: ' + resource.buttonStyle.padding +
+		'; margin: ' + resource.buttonStyle.margin +
         '; text-align: ' + resource.buttonStyle.textAlign +
         '; text-decoration: ' + resource.buttonStyle.textDecoration +
         '; font-size: ' + resource.buttonStyle.fontSize +
@@ -101,7 +89,7 @@ router.post('/deeplink', async (req, res) => {
         '; cursor: ' + resource.buttonStyle.cursor +
 		';';
 	  
-	const html = '<a href="' + resource.linkText + '" target="_blank" style="' + style + '" rel="noopener">' + resource.buttonText + '</a>';
+	const html = '<div><a href="' + resource.linkText + '" target="_blank" style="' + style + '" rel="noopener">' + resource.buttonText + "</a></div><br>";
 
 	const items = {
       type: 'html',
@@ -120,41 +108,41 @@ router.post('/deeplink', async (req, res) => {
 })
 
 // Return available deep linking resources (1st)
-router.get('/resources', async (req, res) => {
-  routesDebug('routes.js -> /resources')
-  const resources = [
-    {
-      name: 'Resource1',
-      value: 'value1'
-    },
-    {
-      name: 'Resource2',
-      value: 'value2'
-    },
-    {
-      name: 'Resource3',
-      value: 'value3'
-    }
-  ]
-  return res.send(resources)
-})
+//router.get('/resources', async (req, res) => {
+//  //routesDebug('routes.js -> /resources')
+//  const resources = [
+//    {
+//      name: 'Resource1',
+//      value: 'value1'
+//    },
+//    {
+//      name: 'Resource2',
+//      value: 'value2'
+//    },
+//    {
+//      name: 'Resource3',
+//      value: 'value3'
+//    }
+//  ]
+//  return res.send(resources)
+//})
 
 // Get user and context information
-router.get('/info', async (req, res) => {
-  const token = res.locals.token
-  const context = res.locals.context
-
-  const info = { }
-  if (token.userInfo) {
-    if (token.userInfo.name) info.name = token.userInfo.name
-    if (token.userInfo.email) info.email = token.userInfo.email
-  }
-
-  if (context.roles) info.roles = context.roles
-  if (context.context) info.context = context.context
-
-  return res.send(info)
-})
+//router.get('/info', async (req, res) => {
+//  const token = res.locals.token
+//  const context = res.locals.context
+//
+//  const info = { }
+//  if (token.userInfo) {
+//    if (token.userInfo.name) info.name = token.userInfo.name
+//    if (token.userInfo.email) info.email = token.userInfo.email
+//  }
+//
+//  if (context.roles) info.roles = context.roles
+//  if (context.context) info.context = context.context
+//
+//  return res.send(info)
+//})
 
 // Wildcard route to deal with redirecting to React routes
 router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
